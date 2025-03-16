6223be02f9866a4865396191e359d34b73de2143
Conor <1421446269@qq.com>
2025-03-16 23:37:25 +1100

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,31 @@
 import type { Metadata } from "next";
-import {IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
-import {cn} from "@/lib/utils";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-ibm-plex",
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
   title: "ImgGo",
-  description: "AI-powered image generator v1",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn("font-IBMPlex antialiasing",IBMPlex.variable)}
-      >
+        <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
         {children}
-      </body>
-    </html>
+        </body>
+        </html>
   );
 }
