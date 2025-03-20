babeb501fe5562384e11ded893b2d697fb4c94a4
Conor <1421446269@qq.com>
2025-03-21 01:13:40 +1100

    update

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import {SignUp} from "@clerk/nextjs";
+import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = () => {
-    return <SignUp/>
+    return <SignUp />
 }
+
 export default SignUpPage
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import {UserButton} from "@clerk/nextjs";
-
+import React from "react";
+import { UserButton, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 
 const Home = () => {
     return (
         <div>
-            <p>Home</p>
-            <UserButton afterSignOutUrl="/"/>
+            {/* 用户已登录时显示 Home 页面 */}
+            <SignedIn>
+                <p>Home</p>
+                <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+
+            {/* 用户未登录时，跳转到 SignIn 页面 */}
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
         </div>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
