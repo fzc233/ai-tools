5d211b5a6a45d367c1473c0a3b8a8a300c144c2e
Conor <1421446269@qq.com>
2025-03-20 23:57:16 +1100

    Update page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {UserButton} from "@clerk/nextjs";
+
 
 const Home = () => {
     return (
-        <div>Page</div>
+        <div>
+            <p>Home</p>
+            <UserButton afterSignOutUrl="/"/>
+        </div>
     )
 }
 export default Home
