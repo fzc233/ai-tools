e91ad7221d2437a2c944bcd9b96ff9126815c128
Conor <1421446269@qq.com>
2025-03-21 23:59:35 +1100

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+//sign in and sign up page
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="auth">{children}</main>
+
     )
 }
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+//after login page
+import SideBar from "@/components/shared/SideBar";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
-
+           <SideBar/>
+            {/*{MobileNav}*/}
 
             <div className="root-container">
                 <div className="wrapper">
                     {children}
+                    123
                 </div>
             </div>
 
