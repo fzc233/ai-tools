d919cbd29f6697a62f6628fbeb0a00625de85081
Conor <1421446269@qq.com>
2025-03-24 00:30:37 +1100

    update main page

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,33 +1,81 @@
-import Link from "next/link";
-import  Image from "next/image";
-import {SignIn} from "@clerk/nextjs";
-import {navLinks} from "@/constants";
-import {usePathname} from "next/navigation";
-const SideBar = () => {
-    const pathname = usePathname()
+"use client"
+
+import { navLinks } from '@/constants'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+
+const Sidebar = () => {
+    const pathname = usePathname();
+
     return (
         <aside className="sidebar">
             <div className="flex size-full flex-col gap-4">
                 <Link href="/" className="sidebar-logo">
-                    <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28}/>
+                    <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
                 </Link>
+
                 <nav className="sidebar-nav">
-                    <SignIn>
+                    <SignedIn>
                         <ul className="sidebar-nav_elements">
-                            {navLinks.map((link)=>{
+                            {navLinks.slice(0, 6).map((link) => {
                                 const isActive = link.route === pathname
-                                return(
-                                    <li key={link.route}
-                                        className={`sidebar-nav_elements group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
-                                        {link.label}
+
+                                return (
+                                    <li key={link.route} className={`sidebar-nav_element group ${
+                                        isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                                    }`}>
+                                        <Link className="sidebar-link" href={link.route}>
+                                            <Image
+                                                src={link.icon}
+                                                alt="logo"
+                                                width={24}
+                                                height={24}
+                                                className={`${isActive && 'brightness-200'}`}
+                                            />
+                                            {link.label}
+                                        </Link>
                                     </li>
                                 )
                             })}
                         </ul>
-                    </SignIn>
+
+
+                        <ul className="sidebar-nav_elements">
+                            {navLinks.slice(6).map((link) => {
+                                const isActive = link.route === pathname
+
+                                return (
+                                    <li key={link.route} className={`sidebar-nav_element group ${
+                                        isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                                    }`}>
+                                        <Link className="sidebar-link" href={link.route}>
+                                            <Image
+                                                src={link.icon}
+                                                alt="logo"
+                                                width={24}
+                                                height={24}
+                                                className={`${isActive && 'brightness-200'}`}
+                                            />
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+
+                            <li className="flex-center cursor-pointer gap-2 p-4">
+                                <UserButton afterSignOutUrl='/' showName />
+                            </li>
+                        </ul>
+                    </SignedIn>
+
+
                 </nav>
             </div>
         </aside>
     )
 }
-export default SideBar
+
+export default Sidebar
