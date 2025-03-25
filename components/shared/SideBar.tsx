aca8a694e6e29040dc52d009172ba5866dcf3043
Conor <1421446269@qq.com>
2025-03-26 00:06:01 +1100

    update

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -5,6 +5,8 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import {Button} from "@/components/ui/button";
+
 
 
 const Sidebar = () => {
@@ -71,6 +73,11 @@ const Sidebar = () => {
                         </ul>
                     </SignedIn>
 
+                    <SignedOut>
+       <Button asChild className="button bg-purple-gradient bg-cover">
+           <Link href="/sign-in">Login</Link>
+       </Button>
+                      </SignedOut>
 
                 </nav>
             </div>
